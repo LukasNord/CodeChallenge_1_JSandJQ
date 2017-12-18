c74f54c2bd1419b5d173b77d5189e310aa0a7c0d
Lukas Nord <luke.nord@gmail.com>
2017-12-18 14:17:50 -0600

    removed consolelogging and more whitespace

--- client.js
+++ client.js
@@ -11,19 +11,15 @@ function start(){
   $('#body').on('click','#deleteBtn', deleteItem);
 }//end start
 function addDiv(){
-
   $('#body').append('<div class="red">');
   $('.red').last().append('<p>'+ totalClicks +'</p>');
   $('.red').last().append('<button id="swapBtn">Swap</button><button id="deleteBtn">Delete</button>');
   totalClicks = totalClicks +=1;
-
-  console.log('total clicks: ' + totalClicks);
 }//end addDiv
 function swapColor(){
   console.log('swap color works');
   $(this).parent('div').toggleClass("red yellow");
 }//end swapColor
-
 function deleteItem() {
   console.log('delete this!');
   $(this).parent().remove();
