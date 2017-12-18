72d47dbb6e63f5e470f91c02b9da88c66e979310
Lukas Nord <luke.nord@gmail.com>
2017-12-18 13:54:39 -0600

    refactored code to put the buttons in the div

--- client.js
+++ client.js
@@ -8,17 +8,33 @@ var totalClicks = 0;
 
 function start(){
   $('#generateBtn').on('click', addDiv);
-
+  $('#body').on('click','#swapBtn', swapColor);
+  $('#body').on('click','#deleteBtn', deleteItem);
 
 }//end start
 
 
 function addDiv(){
 
-  $('#body').append('<div class="generateRed">');
-  $('.generateRed').last().append('<p>'+ totalClicks +'</p>');
-  $('#body').last('div').append('<button id="swapBtn">Swap</button><button id="deleteBtn">Delete</button>');
+  $('#body').append('<div class="red">');
+  $('.red').last().append('<p>'+ totalClicks +'</p>');
+  $('.red').last().append('<button id="swapBtn">Swap</button><button id="deleteBtn">Delete</button>');
   totalClicks = totalClicks +=1;
 
   console.log('total clicks: ' + totalClicks);
 }//end addDiv
+
+
+function swapColor(){
+  console.log('swap color works');
+  $(this).parent('div').toggleClass("red yellow");
+
+
+}//end swapColor
+
+function deleteItem() {
+  console.log('delete this!');
+
+
+
+}//end deleteItem
